33a8c143a3d23d349d14d62d6cdc55ad23cf73b6
lassonico <sionvmx@gmail.com>
2023-08-11 20:02:23 -0500

    despliegue

---
+++ app.js
@@ -0,0 +1,73 @@
+import { platos } from "./platos.js";
+
+// -------------- Boton de reserva -----------------
+
+const enlace = document.querySelector('.contenedor-textos a') // boton de reservar
+enlace.addEventListener('click', function(e){
+    e.preventDefault()
+    const seccionScroll = e.target.attributes.href.value;
+    const seccion = document.querySelector(seccionScroll);
+    seccion.scrollIntoView({ behavior: "smooth" });
+})
+
+// -------------- Map de platos ------------
+
+const carta = document.getElementById('menu')
+
+function elementoPlato(plato) {
+    const platoDiv = document.createElement('div');
+    platoDiv.classList.add('card-plato');
+    platoDiv.innerHTML = `
+    <img src='img/platos/plato_${plato.id}.jpg' alt="imagen de ${plato.nombre}">
+    <div class="textos-card">
+        <p class="format-text" ><span class="titulo-plato">${plato.nombre}</span> ......................................... <span class="precio">$${plato.precio}</span></p>
+        <p class="plato-descripcion">${plato.descripcion}</p>
+    </div>
+    `
+    platoDiv.addEventListener('click', () => {
+
+        const mensaje = `Hola, me interesa el plato ${plato.nombre} y quiero pedirlo a domicilio`
+        const telefono = "573228458059";
+        const url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+
+        window.open(url, "_blank");
+        
+    })
+
+    return platoDiv;
+}
+
+function cargarPlatos() {
+    platos.forEach(plato => {
+      const elementoCarta = elementoPlato(plato);
+      carta.appendChild(elementoCarta);
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', cargarPlatos);
+
+// -------------- fin map de img ---------
+
+// ------ ENVIO DEL FORMULARIO -------
+
+const formulario = document.getElementById('formdatos')
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const nombre = document.getElementById('nombre').value;
+    const telefonocliente = document.getElementById('telefono').value;
+    const comentarios = document.getElementById('comentarios').value;
+
+    const mensaje = `Hola, mi nombre es ${nombre} y quiero hacer una reservación, mi numero es ${telefonocliente}. ${comentarios}`
+    const telefono = "573228458059";
+    const url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+
+    window.open(url, "_blank");
+    
+    formulario.reset()
+
+    window.scroll(0,0)
+})
+
---
+++ platos.js
@@ -0,0 +1,52 @@
+const platos  = [
+    {
+        id: 1,
+        nombre: "plato_1",
+        precio: 12000,
+        descripcion: "Arroz con todas las carnes acompañado de pataconas y ensalada"
+    },
+    {
+        id: 2,
+        nombre: "plato_2",
+        precio: 10800,
+        descripcion: "Pollo en salsa BBQ acompañado de pataconas y ensalada"
+    },
+    {
+        id: 3,
+        nombre: "plato_3",
+        precio: 14000,
+        descripcion: "Carne asada al carbón acompañado de pataconas y ensalada"
+    },
+    {
+        id: 4,
+        nombre: "plato_4",
+        precio: 17000,
+        descripcion: "Arroz blanco con camarones acompañado de pataconas y ensalada"
+    },
+    {
+        id: 5,
+        nombre: "plato_5",
+        precio: 13500,
+        descripcion: "Frijoles en bandeja de barro acompañado de pataconas y ensalada"
+    },
+    {
+        id: 6,
+        nombre: "plato_6",
+        precio: 18600,
+        descripcion: "Cazuela de mariscos acompañado de pataconas y ensalada"
+    },
+    {
+        id: 7,
+        nombre: "plato_7",
+        precio: 9000,
+        descripcion: "Caldo de costilla con papa acompañado de arepas y ensalada"
+    },
+    {
+        id: 8,
+        nombre: "plato_8",
+        precio: 14900,
+        descripcion: "Espaguettis en salsa boloñeza acompañado con arroz blanco y tajada de maduro"
+    }
+]
+
+export {platos} 
