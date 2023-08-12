42a2da718ff9ad59a4ab637bed2c4265e6aa8941
lassonico <sionvmx@gmail.com>
2023-08-12 15:24:30 -0500

    Cambios solicitados

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ function elementoPlato(plato) {
         <p class="plato-descripcion">${plato.descripcion}</p>
     </div>
     `
+
+    // <div class="card">
+    //     <div class="card-details">
+    //         <p class="text-title">Card title</p>
+    //         <p class="text-body">Here are the details of the card</p>
+    //     </div>
+    //     <button class="card-button">More info</button>
+    // </div>
+
     platoDiv.addEventListener('click', () => {
 
         const mensaje = `Hola, me interesa el plato ${plato.nombre} y quiero pedirlo a domicilio`
